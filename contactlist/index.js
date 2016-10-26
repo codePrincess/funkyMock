c6d64dbb7012c82822a0cf62178e5aabe6d63535
Manu Rink <manuela.schrenk@gmail.com>
2016-10-26 18:21:24 +0200

    get rid of mandatory query parameters

--- contactlist/index.js
+++ contactlist/index.js
@@ -1,21 +1,14 @@
-module.exports = function(context, req) {
+module.exports = function (context, req) {
 
-    if (req.query.name || (req.body && req.body.name)) {
-        var fs = require('fs');
-        var json = JSON.parse(fs.readFileSync('D:\\home\\site\\wwwroot\\contactlist\\contactlist.json', 'utf8'));
-        
-        context.log('Output Content : \n'+ JSON.stringify(json));
+    var fs = require('fs');
+    var json = JSON.parse(fs.readFileSync('D:\\home\\site\\wwwroot\\contactlist\\contactlist.json', 'utf8'));
 
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: JSON.stringify(json)
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
+    context.log('Output Content : \n' + JSON.stringify(json));
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: JSON.stringify(json)
+    };
+
     context.done();
 };
